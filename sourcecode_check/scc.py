b20ec8c023b1c469b59bb84a57c7ba121179f446
programmer-666 <suharslan@protonmail.ch>
2024-01-21 23:27:22 +0300

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
           modified:   sourcecode_check/scc.py
    
    SQLite3 database connection, creation actions are added to code.
    
    With these codes, a database file named `scc.db` is created. This database, which has a table called 'source_codes', preserves the names, paths and scc codes of the files containing the source codes.

--- sourcecode_check/scc.py
+++ sourcecode_check/scc.py
@@ -10,20 +10,29 @@ from configparser import ConfigParser
 SCC_CONFIG_FILE: str = argv[1]
 SCC_PATHS_FILE: str = argv[2]
 SCC_MAX_LINE: int = 10
+# SCC_MAX_LINE is checks files first 10 lines for scc code
 
 scc_paths: dict = None
 scc_sc_data: list = []
 
-# Read config data
+
 config = ConfigParser()
 config.read(SCC_CONFIG_FILE)
+# Read config data
 
-# Reading source code paths
+
 with open(SCC_PATHS_FILE, 'r', encoding='utf-8') as paths_file:
+    # read source code paths from json file
+    # paths can be relative or absolute
     scc_paths = jload(paths_file)
 
 
-def sourcecode_control():
+def sourcecode_control() -> None:
+    # sourcecode_control function takes paths and reads source codes
+    # opens every file and reads first 10 (scc_max_line) lines
+    # gets scc_codes, absolute paths and source code file name
+    # all data goest to the scc_sc_data variable
+    # returns None
     for source_code in scc_paths['source_code_paths']:
         with open(source_code, 'r') as src:
             scc_sc_data.append([(scc_code[2:-1],
@@ -36,17 +45,33 @@ def sourcecode_control():
                                 in scc_code][0])
 
 
-class SccSQLite3:
-    def __init__(self):
-        with sqlite3.connect('scc.db') as tmp_db_connection:
-            tdbc_cursor = tmp_db_connection.cursor()
-            tdbc_cursor.execute('CREATE TABLE IF NOT EXISTS "source_codes" \
-                ("id_sc" integer NOT NULL,"path_sc" text NOT NULL, "name_sc" \
-                varchar NOT NULL DEFAULT NULL, "code_sc" varchar NOT NULL, \
-                PRIMARY KEY (id_sc))')
+class SCQlite(sqlite3.Connection):
+    def cursor(self):
+        return super(SCQlite, self).cursor(SCQCursor)
+
+
+class SCQCursor(sqlite3.Cursor):
+    def create_db(self):
+        self.execute('CREATE TABLE IF NOT EXISTS "source_codes" \
+                ("id_sc" integer NOT NULL, "path_sc" \
+                text NOT NULL, "name_sc" varchar NOT NULL DEFAULT NULL, \
+                "code_sc" varchar NOT NULL, PRIMARY KEY (id_sc));')
+        # creating source_codes tables
+        self.execute('CREATE UNIQUE INDEX IF NOT EXISTS \
+            path_sc_ix ON source_codes (path_sc);')
+        # indexes
+
+    def insert_paths(self, sc_data: list):
+        self.executemany('INSERT OR IGNORE INTO source_codes \
+            (code_sc, path_sc, name_sc) VALUES (?, ?, ?)', sc_data)
 
 
 if __name__ == '__main__':
-    #SccSQLite3()
+    db_connection: SCQlite = sqlite3.connect('scc.db', factory=SCQlite)
+    cursor = db_connection.cursor()
+    cursor.create_db()
+
     sourcecode_control()
+    cursor.insert_paths(scc_sc_data)
     print(scc_sc_data)
+    db_connection.commit()
