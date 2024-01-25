abec737218a3b39393fe8d74f8cd6aaba02f0dc1
programmer-666 <suharslan@protonmail.ch>
2024-01-25 23:39:57 +0300

    scc database stuff

--- sourcecode_check/scc.py
+++ sourcecode_check/scc.py
@@ -45,29 +45,67 @@ def sourcecode_control() -> None:
                                 in scc_code][0])
 
 
-def sourcecode_sc_inofs():
-    print([s for s in stat('../app.py')])
+def sourcecode_sc_stats():
+    sc_stats: list = []
+    for sc_element in scc_sc_data:
+        sc_stats.append(tuple(stat(sc_element[1])))
+    return sc_stats
 
 
 class SCQlite(sqlite3.Connection):
+    # Custom SQLite3 inheritence for scc.db
+    # It was defined to define the methods to
+    # be used with the cursor and to simplify operations.
     def cursor(self):
         return super(SCQlite, self).cursor(SCQCursor)
 
 
 class SCQCursor(sqlite3.Cursor):
+    # Cursor
     def create_db(self):
+        # Cursor has few methods for creating and managing scc.db
+        # database have three tables:
+        #   source_codes: sc paths, names and sc codes are stored here.
+        #   sc_file_details: this table have details about source code file
+        #   sc_version: version codes & scc's last run datetime stored here
         self.execute('CREATE TABLE IF NOT EXISTS "source_codes" \
                 ("sc_id" integer NOT NULL, "sc_path" \
                 text NOT NULL, "sc_name" varchar NOT NULL DEFAULT NULL, \
                 "sc_code" varchar NOT NULL, PRIMARY KEY (sc_id));')
-        # creating source_codes tables
+        # SOURCE_CODES
+        self.execute('CREATE TABLE IF NOT EXISTS "sc_file_stats" \
+                ("sc_file_stat_id" integer NOT NULL PRIMARY KEY,\
+                "sc_id" integer NOT NULL,\
+                "sc_file_stat_mode" bigint NOT NULL,\
+                "sc_file_stat_ino" bigint NOT NULL,\
+                "sc_file_stat_dev" bigint NOT NULL,\
+                "sc_file_stat_nlink" bigint NOT NULL,\
+                "sc_file_stat_uid" bigint NOT NULL,\
+                "sc_file_stat_gid" bigint NOT NULL,\
+                "sc_file_stat_size" bigint NOT NULL,\
+                "sc_file_stat_atime" bigint NOT NULL,\
+                "sc_file_stat_mtime" bigint NOT NULL,\
+                "sc_file_stat_ctime" bigint NOT NULL,\
+                FOREIGN KEY(sc_id)REFERENCES source_codes(sc_id));')
+        # SC_FILE_STATS
+        self.execute('CREATE TABLE IF NOT EXISTS "sc_version" \
+                ("sc_version_code" varchar NOT NULL, \
+                "sc_version_c_datetime" datetime NOT NULL, \
+                PRIMARY KEY (sc_version_code));')
+        # SC_VERSION
         self.execute('CREATE UNIQUE INDEX IF NOT EXISTS \
-            sc_path_ix ON source_codes (sc_path);')
+                sc_path_ix ON source_codes (sc_path);')
+        self.execute('CREATE UNIQUE INDEX IF NOT EXISTS \
+                sc_file_stat_id_ix ON sc_file_stats (sc_file_stat_id);')
         # indexes
 
     def insert_paths(self, sc_data: list):
         self.executemany('INSERT OR IGNORE INTO source_codes \
             (sc_code, sc_path, sc_name) VALUES (?, ?, ?);', sc_data)
+
+    def insert_stats(self, sc_stats: list):
+        self.executemany('INSERT OR IGNORE INTO sc_file_stats \
+            (sc_code, sc_path, sc_name) VALUES (?, ?, ?);', sc_stats)
 
 
 if __name__ == '__main__':
@@ -82,4 +120,4 @@ if __name__ == '__main__':
     db_connection.commit()
     # commit changes to database
 
-    print(sourcecode_sc_inofs())
+    print(sourcecode_sc_stats())
