1b75493a7247a6e4c98e701ed46083c1265e3725
programmer-666 <suharslan@protonmail.ch>
2024-01-31 12:13:44 +0300

    code revision

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 #!.venv/bin/python
 # app.py
 # :scc:1:1001:
+
 from __init__ import pvei
 from threading import Thread
 from sourcecode_check import scc
 
 
-scc.activate_scc()
+scc_thread = Thread(target=scc.activate_scc, name='SCC Thread')
+scc_thread.start()
 
-
-def main_t():
-    pvei.proxmox_version()
-    pvei.basic_information()
-    pvei.basic_status()
-
-
-thread = Thread(target=main_t, name="main function", )
-thread.start()
-thread.join()
+pvei.proxmox_version()
+pvei.basic_information()
+pvei.basic_status()
