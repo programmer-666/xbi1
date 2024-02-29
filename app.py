571ead585e9ea3f891ca3452219a145328490072
programmer-666 <suharslan@protonmail.ch>
2024-02-29 20:09:53 +0300

    revision, some improvements & timed task stuff

--- app.py
+++ app.py
@@ -15,14 +15,22 @@ with open('dc_ids.json') as dcj_file:
 # dc_ids.json file holds guild and channel id's
 # bot interact channels with theese data
 
-with open('sce_tasks.json') as scet:
-    sce_tasks: dict = json.load(scet)
+with open('timed_tasks.json') as timet:
+    t_tasks: dict = json.load(timet)
 # loads time and command data for scheduled tasks
 
-commands_em_table: dict = {
+em_messages_table: dict = {
     "version": pveiembeds.em_proxmox_version(pvei.proxmox_version()),
-    "b_status": pveiembeds.em_basic_all_status(pvei.basic_all_status())
+    "b_status": pveiembeds.em_basic_all_status(pvei.basic_all_status()),
+    "b_information": pveiembeds.em_basic_information(pvei.basic_information()),
+    "machines": pveiembeds.em_all_machines(pvei.all_machines())
 }
+# embed messages dict for scheduled tasks
+
+messages_table: dict = {
+    "mtables": pveimessages.all_machines_table(pvei.all_machines())
+}
+# embed messages dict for scheduled tasks
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -38,46 +46,53 @@ dtime = datetime.now()
 async def timed_tasks() -> None:
     global dtime
 
-    # sce_tasks.json reading ...(scheduled tasks-commands)
+    # there is a counter logic here
+    # dtime holds last time
+    # t_tasks function checks every second dtimes if its up to date
 
     if datetime.now().hour > dtime.hour:
         # checks last datetime every second
-        # then starts sending messages to channels
+        # if datetime is updated it updates dtime variable
         dtime = datetime.now()
 
-        await bot.wait_until_ready()
-
-
-        """
-        for guild in list(dc_ids):
-            notf_channel = bot.get_channel(dc_ids[guild]['id_notfc'])
-
-            await notf_channel.send(
-                embed=pveiembeds.em_basic_all_status(
-                    pvei.basic_all_status()
-                )
-            )
-        """
-        # takes every channel id and sends messages
+        for command in em_messages_table:
+            for sce_command in t_tasks['hourly']['commands']:
+                if command == sce_command:
+                    for channel in t_tasks['hourly']['channels']:
+                        notf_channel = bot.get_channel(channel)
+                        await bot.wait_until_ready()
+                        await notf_channel.send(
+                            embed=em_messages_table[command]
+                        )
 
     if datetime.now().minute > dtime.minute:
         # same function but works minutely
         dtime = datetime.now()
 
-        await bot.wait_until_ready()
-
-        for command in commands_em_table:
-            for sce_command in sce_tasks['minutely']['commands']:
+        for command in em_messages_table:
+            for sce_command in t_tasks['minutely']['commands']:
                 if command == sce_command:
-                    for channel in sce_tasks['minutely']['channels']:
+                    for channel in t_tasks['minutely']['channels']:
                         notf_channel = bot.get_channel(channel)
-
+                        await bot.wait_until_ready()
                         await notf_channel.send(
-                            embed=commands_em_table[command]
+                            embed=em_messages_table[command]
                         )
+        # for embed messages
+
+        for command in messages_table:
+            for sce_command in t_tasks['minutely']['commands']:
+                if command == sce_command:
+                    for channel in t_tasks['minutely']['channels']:
+                        notf_channel = bot.get_channel(channel)
+                        await bot.wait_until_ready()
+                        await notf_channel.send(
+                            messages_table[command]
+                        )
+        # for not embed messages
 
 
-# timed_tasks function works every 1 second
+# t_tasks function works every 1 second
 # and checks hourly, minutely jobs
 # this is look not good but now it works, will be update...
 
